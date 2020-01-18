3c078c5d54027446e8efdfb80dd92967df5f66b5
Brittany Russell <sincerelybr@gmail.com>
2020-01-18 19:58:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function add (a,b){
+  var addition = a+b;
+  return addition
+}
+console.log(add)
+
+function subtract (a,b){
+  var subtract = a-b;
+  return subtract
+}
+console.log(subtract)
+
+function multiply (a,b){
+  var multiply = a*b;
+  return multiply
+}
+console.log(multiply)
